43a839bcb6b54cdedb27141e8ba81055c5938456
minji0123 <sso02027@naver.com>
2022-02-26 15:29:41 +0900

    초기설정

---
+++ src/main/java/com/movie/reviewsite/ReviewsiteApplication.java
@@ -0,0 +1,13 @@
+package com.movie.reviewsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/reviewsite/ReviewsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.reviewsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
