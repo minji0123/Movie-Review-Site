f8af498ac3485ab5bd1edd7569402d0c1c2b90a7
YOUNGWOO KIM <partan5260@naver.com>
2022-02-28 10:56:59 +0900

    grade 컨트롤러,서비스,엔티티 crud 수정

--- src/main/java/com/movie/reviewsite/grade/GradeService.java
+++ src/main/java/com/movie/reviewsite/grade/GradeService.java
@@ -1,5 +1,6 @@
 package com.movie.reviewsite.grade;
 
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Slf4j
 @Service
+@ToString
 public class GradeService {
 
     @Autowired
@@ -17,6 +19,49 @@ public class GradeService {
         return gradeRepository.findAll();
     }
 
+    public GradeEntity show(Long id) {
+        return gradeRepository.findById(id).orElse(null);
+    }
 
+    public GradeEntity create(GradeDto dto) {
+        GradeEntity gradeEntity = dto.toEntity();
+        if(gradeEntity.getId() != null){
+            return null; // 생성시 id 중복 방지
+        }
+        return gradeRepository.save(gradeEntity);
+    }
 
+//    public GradeEntity update(Long id, GradeDto dto) {
+//        GradeEntity gradeEntity = dto.toEntity();
+//
+//        GradeEntity target = gradeRepository.findById(id).orElse(null);
+//        if(target == null || id != gradeEntity.getId()){
+//            return null; // 잘못된 요청처리 (원하는대상이 없거나 id가 틀렸을때)
+//        }
+//        target.patch(gradeEntity);
+//        GradeEntity updated = gradeRepository.save(target);
+//        return updated;
+//
+//    }
+    public GradeEntity update(Long id, GradeDto dto) {
+        GradeEntity gradeEntity = dto.toEntity();
+
+        GradeEntity target = gradeRepository.findById(id).orElse(null);
+
+        if (target == null || !id.equals(gradeEntity.getId())){
+            return null;  // 잘못된 요청 응답
+        }
+        target.patch(gradeEntity);
+        return gradeRepository.save(target);
+    }
+
+    public GradeEntity delete(Long id) {
+        GradeEntity target = gradeRepository.findById(id).orElse(null);
+
+        if(target == null){
+            return null; // 잘못된 요청 처리(대상 id가 없을때)
+        }
+        gradeRepository.delete(target);
+        return target;
+    }
 }
