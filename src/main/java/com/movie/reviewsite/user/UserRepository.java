93d9e127cf6d157d59a4c4bba6aa47564fe434ea
minji0123 <sso02027@naver.com>
2022-03-14 14:34:41 +0900

    인증, 인가 제거

--- src/main/java/com/movie/reviewsite/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.movie.reviewsite.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository  extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUsername(String name);
-}
