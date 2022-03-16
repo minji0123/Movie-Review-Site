8e953d9fb553184466448cd8eaac052864de9b74
minji0123 <sso02027@naver.com>
2022-03-16 15:32:04 +0900

    readme 수정

--- src/main/java/com/movie/reviewsite/config/WebMvcConfig.java
+++ src/main/java/com/movie/reviewsite/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.movie.reviewsite.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     // todo: 허락해줄 경로를 설정해줌. 앞에 file:/// 를 붙여줌
     String PermittedPath = "file:///" +System.getProperty("user.dir") + "/src/main/resources/static/";
 
+//    @Value("${uploadPath}")
+//    String path;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**") // PermittedPath 경로들/+a
                 .addResourceLocations(PermittedPath); // 로컬에 저장된 파일을 읽어 올 root 경로 설정
+//                .addResourceLocations(path); // 로컬에 저장된 파일을 읽어 올 root 경로 설정
     }
 }
